286fbe20604b827a141eaa41a9a174f3f92b26f6
Fredric Hegland <114155995+frheg@users.noreply.github.com>
2025-09-14 01:31:42 +0200

    Some update for tests

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'   // Tailwind directives
-import './style.css'   // your custom animations
+import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
