45c7306f9814adc04462a57d38e77ff7f2a9b194
Fredric Hegland <114155995+frheg@users.noreply.github.com>
2025-09-30 14:49:08 +0200

    Typo

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -156,7 +156,7 @@ export default function Sections() {
         <SectionCard id="footer">
           <footer>
             <p>
-              © {new Date().getFullYear()} Fredric Hegland — Last updated:{" "}
+              © {new Date().getFullYear()} Fredric Hegland - Last updated:{" "}
               {new Date(document.lastModified).toLocaleDateString("en-GB", {
                 day: "2-digit",
                 month: "long",
